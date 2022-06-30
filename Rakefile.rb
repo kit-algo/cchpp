d8b3d3fe8e7bc83d44962860b38bb61ed8b30ccd
Tim "tim3z" Zeitz <dev@tim3z.net>
2022-06-30 12:52:24 +0200

    fixes for stuttgart

--- Rakefile.rb
+++ Rakefile.rb
@@ -271,7 +271,8 @@ namespace "exp" do
         num_threads = 1
         while num_threads <= Etc.nprocessors
           100.times do
-            sh "RAYON_NUM_THREADS=#{num_threads} cargo run --release --bin cch_preprocessing -- #{graph} > #{exp_dir}/preprocessing/$(date --iso-8601=seconds).json"
+            sleep 1
+            sh "RAYON_NUM_THREADS=#{num_threads} cargo run --release --bin cch_preprocessing #{num_threads == 1 ? '--features cch-disable-par' : ''} -- #{graph} > #{exp_dir}/preprocessing/$(date --iso-8601=seconds).json"
           end
           num_threads *= 2
         end
